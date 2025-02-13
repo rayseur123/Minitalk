69bbedb9fd56aba2b98c078d80b1d4915db684a3
Nicolas Jooris <njooris@student.42lyon.fr>
2025-02-13 11:25:59 +0100

    minitalk

--- utils/client.d
+++
@@ -1,8 +0,0 @@
-utils/client.o: client.c libft/include/libft.h libft/include/ft_printf.h \
-  libft/include/get_next_line.h
-
-libft/include/libft.h:
-
-libft/include/ft_printf.h:
-
-libft/include/get_next_line.h:
