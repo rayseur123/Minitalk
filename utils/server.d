1002339b7b9924f09cbed5b7cac24586f2073ce8
Nicolas Jooris <njooris@student.42lyon.fr>
2025-02-14 14:18:01 +0100

    without utils

--- utils/server.d
+++
@@ -1,8 +0,0 @@
-utils/server.o: server.c libft/include/libft.h libft/include/ft_printf.h \
-  libft/include/get_next_line.h
-
-libft/include/libft.h:
-
-libft/include/ft_printf.h:
-
-libft/include/get_next_line.h:
